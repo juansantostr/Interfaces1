2d972d809d7442e78ae3adfdf72dddb477cd16ff
Juan Santos <juan_santostr@fet.edu.co>
2025-09-13 19:09:19 -0500

    commit

---
+++ Circulo.java
@@ -0,0 +1,27 @@
+public class Circulo implements Figura {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    // getters/setters opcionales
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+}
+
--- Main.java
+++ Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        Figura f1 = new Cuadrado(4);
+        Figura f2 = new Circulo(3);
+
+        System.out.println("Área cuadrado: " + f1.calcularArea());
+        System.out.println("Área círculo: " + f2.calcularArea());
+    }
 }
